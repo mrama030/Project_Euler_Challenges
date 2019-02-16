6b9d02211f6e03d5b034b7e34216253285ae9476
mrama030 <mrama030@uottawa.ca>
2019-02-16 17:47:40 -0500

    Problems 1, 2 and 4 complete

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+// Mohamed Ali Ramadan
+
+// Description:
+// Prints out the solutions to the Project Euler problems.
+
+import EulerSolutions.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Problem 1");
+        int solution_1 = Solutions.Problem_1();
+        System.out.println(solution_1);
+        System.out.println("");
+
+        System.out.println("Problem 2");
+        int solution_2 = Solutions.Problem_2();
+        System.out.println(solution_2);
+        System.out.println("");
+
+        System.out.println("Problem 4");
+        int solution_4 = Solutions.Problem_4();
+        System.out.println(solution_4);
+        System.out.println("");
+    }
+}
